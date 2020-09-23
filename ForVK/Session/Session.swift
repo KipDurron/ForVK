246b6cf40e41c7798f9daeb03c6f1dbc500a0af7
kipdurron <ishunin97@gmail.com>
2020-09-23 21:26:31 +0300

    suds

--- ForVK/Session/Session.swift
+++ ForVK/Session/Session.swift
@@ -2,15 +2,20 @@
 //  Session.swift
 //  ForVK
 //
-//  Created by Илья Кадыров on 27.07.2020.
+//  Created by Илья Кадыров on 23.09.2020.
 //  Copyright © 2020 Илья Ишунин. All rights reserved.
 //
 
 
+
+
+import Foundation
 class Session {
     var token: String = ""
     var userId: Int = 0
     static let instance = Session()
-
     private init(){}
 }
+
+
+

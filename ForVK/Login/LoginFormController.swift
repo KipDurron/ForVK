dcc7aef076eb95f883d0dde83a49515d983c7c00
kipdurron <ishunin97@gmail.com>
2020-09-28 15:14:35 +0300

    commit

--- ForVK/Login/LoginFormController.swift
+++ ForVK/Login/LoginFormController.swift
@@ -10,6 +10,8 @@ import UIKit
 import WebKit
 
 class LoginFormController: UIViewController {
+    
+    var loginService = LoginService()
 
     @IBOutlet weak var webview: WKWebView! {
         didSet{
@@ -68,25 +70,7 @@ class LoginFormController: UIViewController {
     }
 
     func startWebView() {
-        var urlComponents = URLComponents()
-                urlComponents.scheme = "https"
-                urlComponents.host = "oauth.vk.com"
-                urlComponents.path = "/authorize"
-                urlComponents.queryItems = [
-                    URLQueryItem(name: "client_id", value: AuthSetting.clientId.rawValue),
-                    URLQueryItem(name: "display", value: "mobile"),
-                    URLQueryItem(name: "redirect_uri", value: "https://oauth.vk.com/blank.html"),
-                    URLQueryItem(name: "scope", value: "262150"),
-                    URLQueryItem(name: "response_type", value: "token"),
-                    URLQueryItem(name: "v", value: "5.68")
-                ]
-                
-        let request = URLRequest(url: urlComponents.url!)
-        
-        webview.load(request)
-        
-        
-       
+        webview.load(loginService.authVKRequest())
     }
     
     override func viewDidLoad() {
---
+++ ForVK/Login/LoginService.swift
@@ -0,0 +1,27 @@
+//
+//  LoginService.swift
+//  ForVK
+//
+//  Created by Илья Кадыров on 28.09.2020.
+//  Copyright © 2020 Илья Ишунин. All rights reserved.
+//
+
+import Foundation
+
+class LoginService {
+    func authVKRequest() -> URLRequest {
+        var urlComponents = URLComponents()
+                urlComponents.scheme = "https"
+                urlComponents.host = "oauth.vk.com"
+                urlComponents.path = "/authorize"
+                urlComponents.queryItems = [
+                    URLQueryItem(name: "client_id", value: AuthSetting.clientId.rawValue),
+                    URLQueryItem(name: "display", value: "mobile"),
+                    URLQueryItem(name: "redirect_uri", value: "https://oauth.vk.com/blank.html"),
+                    URLQueryItem(name: "scope", value: "262150"),
+                    URLQueryItem(name: "response_type", value: "token"),
+                    URLQueryItem(name: "v", value: "5.68")
+                ]
+        return URLRequest(url: urlComponents.url!)
+    }
+}

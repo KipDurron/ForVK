e8ccffe5330c791d60103ea766e7229fab20278f
kipdurron <ishunin97@gmail.com>
2021-04-09 20:45:00 +0300

    edit gitignorм

---
+++ ForVK/Setting.swift
@@ -0,0 +1,14 @@
+//
+//  Setting.swift
+//  ForVK
+//
+//  Created by Илья Кадыров on 27.09.2020.
+//  Copyright © 2020 Илья Ишунин. All rights reserved.
+//
+
+import Foundation
+
+enum AuthSetting: String {
+    case clientId = "7607991"
+   
+}
